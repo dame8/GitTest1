75bed391f3e060a6f425e61efdf6af45b5ef138f
XX <stefanovski.dame8@gmail.com>
2024-02-28 20:18:40 +0100

    adding new changes

---
+++ src/intedrationtest1/LoginTest1.java
@@ -0,0 +1,13 @@
+package intedrationtest1;
+
+public class LoginTest1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        String a = "dskfcgf";
+        sayHello( name:"Dame");
+        System.out.println("Learning Git...");
+        System.out.println("committing changes using IntelliJ");
+        System.out.println("Reverting...");
+        System.out.println("pushing changes...");
+    }
+}
